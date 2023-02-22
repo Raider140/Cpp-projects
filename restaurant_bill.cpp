dbd715383acaf5c0626c3a6fc9484bd20cc17fa3
Nathan Little <58357881+Raider140@users.noreply.github.com>
2023-02-22 13:54:14 -0500

    Add files via upload

---
+++ restaurant_bill.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    double meal = 88.67;
+    double tax =6.75;
+    double tip = 20;
+    cout << setw(3) << meal;
+    return 0
+
+
+
+
+
+}
