e4575365dd515acf211d455c4e84c825b06cf476
Nathan Little <58357881+Raider140@users.noreply.github.com>
2023-02-22 11:53:23 -0500

    Update and rename homework1_1.cpp to IO_birthyear.cpp

---
+++ IO_birthyear.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(){
+int year;
+printf("Please enter a birth year for Mary: ");
+scanf("%d", &year);
+printf("\nHer name is Mary,\n\n\
+Mary was born in %d", year);
+
+return 0;
+
+}
+
